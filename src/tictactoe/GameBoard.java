84a4438d9a37742f59a1b7f134c1075e66ba75a9
Hana Lee <hanalee07@gmail.com>
2016-07-28 14:41:50 -0500

    Rework GameBoard object

--- src/tictactoe/GameBoard.java
+++ src/tictactoe/GameBoard.java
@@ -3,51 +3,22 @@ package tictactoe;
 import java.util.ArrayList;
 
 public class GameBoard {
-    private ArrayList<String> spaces;
     private int numRows;
+    private int[] spaces;
 
     GameBoard() {
-        numRows = 3;
-        spaces = new ArrayList<String>();
-        for (int n = 0; n < (numRows * numRows); n++) {
-            spaces.add("");
-        }
-        
+        this.numRows = 3;
+        this.spaces = new int[9];
     }
 
-    public ArrayList<String> getBoard() {
-        return spaces;
+    GameBoard(int numRows) {
+        this.numRows = numRows;
+        this.spaces = new int[numRows * numRows];
     }
 
     public int getNumRows() {
         return numRows;
     }
 
-    public int[][] getRows() {
-        int[][] rows = new int[(2 * (numRows + 1))][numRows];
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numRows; j++) {
-                rows[i][j] = 3 * i + j;
-            }
-        }
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numRows; j++) {
-                rows[(i + numRows)][j] = 3 * j + i;
-            }
-        }
-        for (int j = 0; j < numRows; j++) {
-            rows[2 * numRows][j] = j * (numRows + 1);
-            rows[2 * numRows + 1][j] = (j + 1) * 
-                                         (numRows - 1);
-        }
-        return rows;
-    }
-
-    public String getSpace(int space) {
-        return spaces.get(space);
-    }
-    
-    public void move(int space, String marker) {
-        spaces.set(space, marker);
-    }
+    public int[] getSpaces() { return spaces; }
 }
---
+++ test/tictactoe/GameBoardTest.java
@@ -0,0 +1,69 @@
+package tictactoe;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+//import org.junit.Before;
+
+public class GameBoardTest {
+
+//    private GameBoard board;
+//
+//    @Before
+//    public void setUp() {
+//        GameBoard board = new GameBoard();
+//    }
+
+    @Test
+    public void testCreateTwoByTwoBoard() {
+        GameBoard board = new GameBoard(2);
+        assertNotNull(board);
+        assertEquals(2, board.getNumRows());
+        assertEquals(4, board.getSpaces().length);
+    }
+
+    @Test
+    public void testCreateThreeByThreeBoard() {
+        GameBoard board = new GameBoard();
+        assertEquals(9, board.getSpaces().length);
+    }
+
+
+//    @Test
+//    public void testCreateGameBoard() {
+//        int empty = 0;
+//        for (String space : board.getBoard()) {
+//            if (space.equals("")) {
+//                empty++;
+//            }
+//        }
+//        assertEquals(9, empty);
+//    }
+//
+//    @Test
+//    public void testGetSpace() {
+//        assertEquals("", board.getSpace(1));
+//    }
+//
+//    @Test
+//    public void testMove() {
+//        board.move(1, "X");
+//        assertEquals("X", board.getSpace(1));
+//    }
+//
+//    @Test
+//    public void testGetBoardNumRows() {
+//        assertEquals(3, board.getNumRows());
+//    }
+//
+//    @Test
+//    public void testGetBoardRows() {
+//        int[][] rows = {{0, 1, 2}, {3, 4, 5},
+//                        {6, 7, 8}, {0, 3, 6},
+//                        {1, 4, 7}, {2, 5, 8},
+//                        {0, 4, 8}, {2, 4, 6}};
+//        assertArrayEquals(rows, board.getRows());
+//    }
+    
+
+
+}
