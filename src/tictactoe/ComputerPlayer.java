01cda4b1dc8f4f5cd454827036b2c4a7306ed712
Hana Lee <hanalee07@gmail.com>
2016-08-01 16:51:01 -0500

    Implement code to pass test for empty spaces on full board

--- src/tictactoe/ComputerPlayer.java
+++ src/tictactoe/ComputerPlayer.java
@@ -1,12 +1,21 @@
 package tictactoe;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
  * Created by hanalee on 8/1/16.
  */
-public class ComputerPlayer {
+public class ComputerPlayer extends GamePlayer {
     public ArrayList<Integer> getEmptySpaces(GameRecord record) {
-        return record.getBoard().getSpaces();
+        ArrayList<Integer> allSpaces = record.getBoard().getSpaces();
+        ArrayList<Integer> allMoves = record.getAllMoves();
+        ArrayList<Integer> emptySpaces = new ArrayList<Integer>();
+        for (int space : allSpaces) {
+            if (!allMoves.contains(space)) {
+                emptySpaces.add(space);
+            }
+        }
+        return emptySpaces;
     }
 }
--- test/tictactoe/ComputerPlayerTest.java
+++ test/tictactoe/ComputerPlayerTest.java
@@ -3,6 +3,7 @@ package tictactoe;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 
 /**
  * Created by hanalee on 8/1/16.
@@ -19,5 +20,17 @@ public class ComputerPlayerTest {
             emptySpaces.add(space);
         }
         assertEquals(emptySpaces, computer.getEmptySpaces(record));
+    }
+
+    @Test
+    public void testGetEmptySpacesOnFullBoard() {
+        ComputerPlayer computer = new ComputerPlayer();
+        GameBoard board = new GameBoard(3);
+        GameRecord record = new GameRecord(board);
+        for (int space = 0; space < 9; space++) {
+            record.newMove(space, computer);
+        }
+        ArrayList<Integer> emptySpaces = new ArrayList<Integer>();
+        assertEquals(new ArrayList<Integer>(), computer.getEmptySpaces(record));
     }
 }
