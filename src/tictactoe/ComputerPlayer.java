8d0cdb8fc0f58cff4de34673628d4c9c3400b60c
Hana Lee <hanalee07@gmail.com>
2016-08-01 16:45:01 -0500

    Implement minimum code to pass test for empty spaces on empty board

--- src/tictactoe/ComputerPlayer.java
+++ src/tictactoe/ComputerPlayer.java
@@ -1,7 +1,12 @@
 package tictactoe;
 
+import java.util.ArrayList;
+
 /**
  * Created by hanalee on 8/1/16.
  */
 public class ComputerPlayer {
+    public ArrayList<Integer> getEmptySpaces(GameRecord record) {
+        return record.getBoard().getSpaces();
+    }
 }
--- test/tictactoe/ComputerPlayerTest.java
+++ test/tictactoe/ComputerPlayerTest.java
@@ -1,7 +1,23 @@
 package tictactoe;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
 /**
  * Created by hanalee on 8/1/16.
  */
 public class ComputerPlayerTest {
+
+    @Test
+    public void testGetEmptySpacesOnEmptyBoard() {
+        ComputerPlayer computer = new ComputerPlayer();
+        GameBoard board = new GameBoard(3);
+        GameRecord record = new GameRecord(board);
+        ArrayList<Integer> emptySpaces = new ArrayList<Integer>();
+        for (int space = 0; space < 9; space++) {
+            emptySpaces.add(space);
+        }
+        assertEquals(emptySpaces, computer.getEmptySpaces(record));
+    }
 }
