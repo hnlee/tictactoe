4af62f393f420289dd4590f89e2d002dfcc53055
Hana Lee <hanalee07@gmail.com>
2016-08-01 13:20:26 -0500

    Fix test order

--- test/tictactoe/GameControlCenterTest.java
+++ test/tictactoe/GameControlCenterTest.java
@@ -102,11 +102,11 @@ public class GameControlCenterTest {
     public void testAnalyzeWonGame() {
         game.setUp();
         simulateGame(game, 4, 1, 5, 3, 2, 8, 6);
-        game.analyzeBoard();
         assertEquals(true, game.isRowBlocked(new int[] {0, 1, 2}));
         assertEquals(false, game.isRowBlocked(new int[] {2, 4, 6}));
         assertEquals(2, game.getRowOccupancy(new int[] {0, 1, 2}));
         assertEquals(3, game.getRowOccupancy(new int[] {2, 4, 6}));
+        game.analyzeBoard();
         assertEquals("win", game.getStatus());
     }
 
