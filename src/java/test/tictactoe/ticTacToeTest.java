adca3bc5fc5e0ae42e80b9d3ea6240f18fd9046c
Hana Lee <hanalee07@gmail.com>
2016-08-15 14:04:23 -0500

    Make test class for acceptance tests

---
+++ src/java/test/tictactoe/ticTacToeTest.java
@@ -0,0 +1,14 @@
+package tictactoe;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by hanalee on 8/15/16.
+ */
+public class ticTacToeTest {
+
+    @Test
+    public void testMain() {
+    }
+}
