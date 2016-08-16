a5dbaa4b6aba1123befd64041242f92dd486754f
Hana Lee <hanalee07@gmail.com>
2016-08-16 17:17:15 -0500

    Make Board interface

---
+++ src/java/main/tictactoe/Board.java
@@ -0,0 +1,20 @@
+package tictactoe;
+
+import java.util.List;
+
+/**
+ * Created by hanalee on 8/16/16.
+ */
+public interface Board {
+
+    int getNumRows();
+    
+    List<Integer> getSpaces();
+
+    int[][] getRows();
+
+    boolean isSpaceInRow(int space, int[] row);
+
+    int[][] getRowsWithSpace(int space);
+
+}
